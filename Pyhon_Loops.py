697e200e7396deb7704568a0086af1a69bbbf216
Manish Paul <thepaul9650@gmail.com>
2026-08-26 21:52:59 +0530

    Implement loop to filter and display numbers
    Added a loop to display numbers from a list based on specific conditions.

--- Pyhon_Loops.py
+++ Pyhon_Loops.py
@@ -6,3 +6,22 @@ for i in range(1,number+1):
     n += 1 
     print(f"Current number is: {n} and the cube of {n} is: {i}")
 
+ ##Exercise 2. Display numbers from a list using a loop
+    #Given a list of numbers, iterate through it and print numbers that satisfy these conditions:
+        numbers = [12, 75, 150, 180, 145, 525, 50]
+    # 1.The number must be divisible by five.
+    # 2.If the number is greater than 150, skip it and move to the next.
+    # 3.If the number is greater than 500, stop the loop entirely.
+    numbers = [12, 75, 150, 180, 145, 525, 50]
+
+for i in numbers:
+    # To check the number is greater than 500 to break the loop.
+    if i > 500:
+        break
+    # To check the number is greater than 150 to skip it and move to next move.
+    if i > 150:
+        continue
+    # To check the number is divisible by five.
+    if i%5 == 0:
+        print(i)
+    
