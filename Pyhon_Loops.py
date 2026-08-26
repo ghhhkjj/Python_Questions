88f05deeacf9a9203cd35dd6702dd7d0a123b2d8
Manish Paul <thepaul9650@gmail.com>
2026-08-26 22:14:24 +0530

    Update Pyhon_Loops.py

--- Pyhon_Loops.py
+++ Pyhon_Loops.py
@@ -42,6 +42,19 @@
         print(v_count)
         print(c_count)
 
+# Exercise 4. Count total number of digits in a number
+        num = 75869
+        count = 0
+        
+        # Keep dividing by 10 until the number reaches 0
+        while num != 0:
+            # Floor division to remove the last digit
+            num = num // 10
+            # Increment the counter
+            count = count + 1
+        
+        print("Total digits are:", count)
+
 
 
 
